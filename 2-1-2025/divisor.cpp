a831a0f0b46f3366c80c03a169d76c7d25e488a4
noortanha <tanhanoor823@gmail.com>
2025-01-02 23:41:57 +0600

    Add files via upload

---
+++ 2-1-2025/divisor.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+int n;
+   cin >> n;
+   vector<int> divisors;
+   for (int i = 1;i * i <= n;i++) {
+      if (n % i == 0) {
+         divisors.push_back(i);
+         if ((n / i) != i) {
+            divisors.push_back(n / i);
+         }
+      }
+   }
+
+   sort(divisors.begin(), divisors.end());
+   for (auto value : divisors) {
+      cout << value << " ";
+   }
+   cout << endl;
+
+   return 0;
+}
